226ed35b607d3920411c8a1a2187f4bc8f31b494
arbazahmed07 <mdarbazking7@gmail.com>
2025-07-09 01:09:45 +0530

    comment code which not needed

--- app/api/serp/route.ts
+++ app/api/serp/route.ts
@@ -1,38 +1,38 @@
-import axios from 'axios';
-import { NextRequest,NextResponse } from 'next/server';
-type SearchResult={
-  title: string;
-  link: string;
-  snippet: string;
-}
-type SerpApiResponse = {
-  organic_results?: SearchResult[];
-  [key: string]: any; // To allow other properties from the API response
-}
-export  async function GET(req: NextRequest) {
-  const {searchParams}= new URL(req.url);
+// import axios from 'axios';
+// import { NextRequest,NextResponse } from 'next/server';
+// type SearchResult={
+//   title: string;
+//   link: string;
+//   snippet: string;
+// }
+// type SerpApiResponse = {
+//   organic_results?: SearchResult[];
+//   [key: string]: any; // To allow other properties from the API response
+// }
+// export  async function GET(req: NextRequest) {
+//   const {searchParams}= new URL(req.url);
 
-    console.log('Received request:', req);
-  const query= searchParams.get('query');
-    console.log('Received query:', query);
-  if (!query || typeof query !== 'string') {
-    return NextResponse.json({ error: 'Invalid or missing query parameter' }, { status: 400 });
-  }
-  console.log('Received query:', query);
+//     console.log('Received request:', req);
+//   const query= searchParams.get('query');
+//     console.log('Received query:', query);
+//   if (!query || typeof query !== 'string') {
+//     return NextResponse.json({ error: 'Invalid or missing query parameter' }, { status: 400 });
+//   }
+//   console.log('Received query:', query);
 
-  try {
-    const response = await axios.get<SerpApiResponse>('https://serpapi.com/search.json',{
-      params: {
-        q: query,
-        api_key: process.env.SERP_API_KEY, 
-        engine: 'google'
-      },
-    });
-    return NextResponse.json(response.data);
-  } catch (error:any) {
-    console.error('Error fetching data from SerpAPI:', error.message);
-    return NextResponse.json({ error: 'Failed to fetch data from SerpAPI' }, { status: 500 });
-  }
+//   try {
+//     const response = await axios.get<SerpApiResponse>('https://serpapi.com/search.json',{
+//       params: {
+//         q: query,
+//         api_key: process.env.SERP_API_KEY, 
+//         engine: 'google'
+//       },
+//     });
+//     return NextResponse.json(response.data);
+//   } catch (error:any) {
+//     console.error('Error fetching data from SerpAPI:', error.message);
+//     return NextResponse.json({ error: 'Failed to fetch data from SerpAPI' }, { status: 500 });
+//   }
 
 
-}
+// }
